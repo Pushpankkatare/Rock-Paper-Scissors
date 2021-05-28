a5c2a9551d3e485a36597c6a312e38f5f5311c16
Pushpank <pushpankkatare09@gmail.com>
2021-05-28 23:45:53 +0530

    semi-final

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log(Math.random());
-
 
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
@@ -7,35 +5,48 @@ const scissor = document.querySelector('.scissor');
 const u_score = document.querySelector('#u_score');
 const c_score = document.querySelector('#c_score');
 const selected = document.querySelector('.selection');
+const selected2 = document.querySelector('.selection2');
 const a = document.querySelector('#a');
 const b = document.querySelector('#b');
 const c = document.querySelector('#c');
-let user=0, comp=0;
+let user=1, comp=1;
+let comp_choice,user_choice;
+const rps = [rock, paper, scissor];
+console.log(rps[0]);
 
-
-function score_board(s)
+function score_board(s, user_choice)
 {
-    if(s==0)
-    return;
+    if(s==0){
+        rps[user_choice-1].classList.add('silver-glow');
+        setTimeout(function(){
+            rps[user_choice-1].classList.remove('silver-glow');
+        }, 500);
+        return;
+    }
     else if(s==-1){
         comp++;
         c_score.innerHTML=comp;
+        rps[user_choice-1].classList.add('red-glow');
+        setTimeout(function(){
+            rps[user_choice-1].classList.remove('red-glow');
+        }, 500);
     }
     else{
         user++;
         u_score.innerHTML=user;
+        rps[user_choice-1].classList.add('green-glow');
+        setTimeout(function(){
+            rps[user_choice-1].classList.remove('green-glow');
+        }, 500);
     }
 }
 
 
 function Comp_Choice(){
     let n = Math.random();
-    if(n<0.333)
-    return 1;
-    else if(n>0.666)
-    return 3;
-    else
-    return 2;
+    n = Math.floor(n*3) + 1;
+    console.log(n);
+    return n;
 }
 
 function who_won(u,c){
@@ -66,52 +77,34 @@ function who_won(u,c){
     }
 }
 
-const choice = document.querySelector('.choice');
-choice.innerHTML= "Rock destroys scissor";
+function colorx(ch){
+    if(ch==1){
+        selected2.innerHTML='Rock';
+        selected2.style.color = "rgb(239,84,128)";
+    }
+    else if(ch==2){
+        selected2.innerHTML='Paper';
+        selected2.style.color = "rgb(255,222,85)";
+    }
+    else{
+        selected2.innerHTML='Scissor';
+        selected2.style.color = "rgb(116,218,226)";
+    }
+}
+
+// const choice = document.querySelector('.choice');
+// choice.innerHTML= "Rock destroys scissor";
 
 rock.addEventListener('click', function(){
     selected.innerHTML='Rock';
     selected.style.color = "rgb(239,84,128)";
     a.style.color = "rgb(239,84,128)";
-    const user_choice = 1;
-    const comp_choice = Comp_Choice();
+    user_choice = 1;
+    comp_choice = Comp_Choice();
+    colorx(comp_choice);
     const res = who_won(user_choice, comp_choice);
-    score_board(res);
-    if(res==1){
-        rock.style.borderColor="rgb(15, 214, 58)";
-        rock.style.transition= ".5s ease";
-    }
-    else if(res==-1){
-        rock:hover.style.borderColor="red";
-        rock:hover.style.transition= ".5s ease";
-    }
-    else{
-        rock.style.borderColor="yellow";
-        rock.style.transition= ".5s ease";
-    }
-    console.log(who_won(user_choice, comp_choice));
-});
+    score_board(res, user_choice);
 
-rock.addEventListener('click', function(){
-    selected.innerHTML='Rock';
-    selected.style.color = "rgb(239,84,128)"
-    const user_choice = 1;
-    const comp_choice = Comp_Choice();
-    const res = who_won(user_choice, comp_choice);
-    score_board(res);
-    if(res==1){
-        rock.style.borderColor="rgb(15, 214, 58)";
-        rock.style.transition= ".5s ease";
-    }
-    else if(res==-1){
-        rock.style.borderColor="red";
-        rock.style.transition= ".5s ease";
-    }
-    else{
-        rock.style.borderColor="yellow";
-        rock.style.transition= ".5s ease";
-    }
-    console.log(who_won(user_choice, comp_choice));
 });
 
 
@@ -119,23 +112,11 @@ paper.addEventListener('click', function(){
     selected.innerHTML='Paper';
     selected.style.color = "rgb(255,222,85)";
     b.style.color = "rgb(255,222,85)";
-    const user_choice = 1;
-    const comp_choice = Comp_Choice();
+    user_choice = 2;
+    comp_choice = Comp_Choice();
+    colorx(comp_choice);
     const res = who_won(user_choice, comp_choice);
-    score_board(res);
-    if(res==1){
-        paper.style.borderColor="rgb(15, 214, 58)";
-        paper.style.transition= ".5s ease";
-    }
-    else if(res==-1){
-        paper.style.borderColor="red";
-        paper.style.transition= ".5s ease";
-    }
-    else{
-        paper.style.borderColor="white";
-        paper.style.transition= ".5s ease";
-    }
-    console.log(who_won(user_choice, comp_choice));
+    score_board(res, user_choice);
 });
 
 
@@ -143,21 +124,9 @@ scissor.addEventListener('click', function(){
     selected.innerHTML='Scissor';
     selected.style.color = "rgb(116,218,226)";
     c.style.color = "rgb(116,218,226)";
-    const user_choice = 1;
-    const comp_choice = Comp_Choice();
+    user_choice = 3;
+    comp_choice = Comp_Choice();
+    colorx(comp_choice);
     const res = who_won(user_choice, comp_choice);
-    score_board(res);
-    if(res==1){
-        scissor.style.borderColor="rgb(15, 214, 58)";
-        scissor.style.transition= ".5s ease";
-    }
-    else if(res==-1){
-        scissor.style.borderColor="red";
-        scissor.style.transition= ".5s ease";
-    }
-    else{
-        scissor.style.borderColor="white";
-        scissor.style.transition= ".5s ease";
-    }
-    console.log(who_won(user_choice, comp_choice));
+    score_board(res, user_choice);
 });
